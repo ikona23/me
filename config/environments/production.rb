57980efa687cc47e49f461b6a5b036cfab3ef429
ikona23@gmail.com <ikona23@gmail.com>
2014-10-25 00:23:49 +1300

    hope

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,10 @@ Me::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
+  config.assets.compile = ['*.js', '*.css']
+
+  config.active_support.deprecation = :silence
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -15,7 +19,7 @@ Me::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
